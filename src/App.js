2bcfdeb4c73a0db9055c26f874fd010b4c93e6fd
shraddh borade <shraddhacareer99@gmail.com>
2023-01-31 21:29:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Coin from './Coin';
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+  axios.get(url).then(res => {
+    console.log(res);
+    setCoins(res.data)
+  })
+
+  useEffect(() => {
+
+  }, []);
+
+  const getInputValue = (e) => {
+    setSearch(e.target.value)
+  }
+  const filterCoins = coins.filter(coin => {
+    return coin.name.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="ApiName">Cryptocurrency API</h1>
+      <input type="text" placeholder='Search' onChange={getInputValue} style={{width:"30rem",height:"50px"}} />
+
+
+      <div className='coins-container'>
+        {
+          filterCoins.map((coin) => {
+            return <Coin image={coin.image} name={coin.name} symbol={coin.symbol}
+              current_price={coin.current_price} total_volume={coin.total_volume} />
+          })
+        }
+      </div>
     </div>
   );
 }
